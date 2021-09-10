fe3f58a48fb4cfc358b8a45cc0252cd01d9fdcd6
xXHachimanXx <globaldep90@gmail.com>
2021-09-09 21:12:45 -0300

    Initializing project

---
+++ API-for-point-management-and-access-control/src/main/java/com/dio/APIforpointmanagementandaccesscontrol/ApiForPointManagementAndAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.dio.APIforpointmanagementandaccesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForPointManagementAndAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForPointManagementAndAccessControlApplication.class, args);
+	}
+
+}
